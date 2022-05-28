5986730e94bbcfae56f79b27e6d103dc01b4dfee
Eric Gross <eric@vw-dilab.com>
2022-05-28 23:04:56 +0200

    Add TourCard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import {Box, Paper} from "@mui/material";
+import TourCard from "./components/TourCard";
 
 function App() {
   return (
     <div className="App">
+        <TourCard/>
     </div>
   );
 }
---
+++ src/components/TourCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Paper} from "@mui/material";
+
+export default function TourCard() {
+    return (
+        <div>
+            <Paper elevation={3}>Hello</Paper>
+        </div>
+    );
+};
