43634d91c0c1b3af54bc392a6ddc9aa764732b89
Eric Gross <eric@vw-dilab.com>
2022-05-30 00:36:59 +0200

    Lesson content

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Container, Grid, Typography} from "@mui/material";
+import cities from "../data.json";
+import TourCard from "../components/TourCard";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <Container sx={{marginY: 5}}>
+                {cities.map((city) => (
+                    <>
+                        <Typography
+                            variant={"h4"}
+                            component={"h2"}
+                            marginTop={5}
+                            marginBottom={3}
+                        >
+                            Top {city.name} Tours
+                        </Typography>
+                        <Grid container spacing={5}>
+                            {city.tours.map((tour, index) => <TourCard tour={tour} key={index}/>)}
+                        </Grid>
+                    </>
+                ))}
+
+            </Container>
+        </div>
+    );
+};
