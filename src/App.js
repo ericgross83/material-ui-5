afa625c768952cdc1327e0404854ea326a25261c
Eric Gross <eric@vw-dilab.com>
2022-05-30 00:23:11 +0200

    Lesson content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import {Container, Grid} from "@mui/material";
-import TourCard from "./components/TourCard";
+import {Container, Grid, Typography} from "@mui/material";
 import * as PropTypes from "prop-types";
+import SearchAppBar from "./components/AppBar";
+import cities from './data.json'
+import TourCard from "./components/TourCard";
 
 function Item(props) {
     return null;
@@ -12,13 +14,24 @@ Item.propTypes = {children: PropTypes.node};
 function App() {
     return (
         <div className="App">
-            <Container>
-                <Grid container spacing={5}>
-                    <TourCard/>
-                    <TourCard/>
-                    <TourCard/>
-                    <TourCard/>
-                </Grid>
+            <SearchAppBar/>
+            <Container sx={{marginY: 5}}>
+                {cities.map((city) => (
+                    <>
+                    <Typography
+                        variant={"h4"}
+                        component={"h2"}
+                        marginTop={5}
+                        marginBottom={3}
+                    >
+                        Top {city.name} Tours
+                    </Typography>
+                        <Grid container spacing={5}>
+                            {city.tours.map((tour,index) => <TourCard tour={tour} key={index}/>)}
+                        </Grid>
+                    </>
+                ))}
+
             </Container>
         </div>
     );
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import {Box, Grid, Paper, Rating, Typography} from "@mui/material";
+import {Box, createTheme, Grid, Paper, Rating, ThemeProvider, Typography} from "@mui/material";
 import {AccessTime} from "@mui/icons-material";
 
-export default function TourCard() {
-    const boxStyles = {
-        display: "flex",
-        alignItems: "center"
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants: [{
+                props: {
+                    variant: "body2"
+                }, style: {
+                    fontSize: 11
+                }
+            }, {
+                props: {
+                    variant: "body3"
+                }, style: {
+                    fontSize: 9
+                }
+            },]
+        }
     }
-    return (
-        <Grid item xs={3}>
-            <Paper elevation={3}>
-                <img className={'img'} src={'https://picsum.photos/200'}/>
-                <Box paddingX={1}>
-                    <Typography variant={'subtitle1'} component={'h2'}>
-                        Immerse into the Falls
-                    </Typography>
-                    <Box
-                        sx={boxStyles}
-                    >
-                        <AccessTime sx={{width: 12.5}}/>
-                        <Typography variant={'body2'} component={'p'} marginLeft={0.5}>
-                            5 hours
-                        </Typography>
-                    </Box>
-                    <Box sx={boxStyles} marginTop={3}>
-                        <Rating name="read-only" value={4.5} precision={0.5} readOnly size={"small"} />
-                        <Typography variant={"body2"} component={"p"}>4.5</Typography>
-                        <Typography variant={"body3"} component={"p"} marginLeft={1.5}>(655 reviews)</Typography>
-                    </Box>
-                    <Box>
-                        <Typography variant={"h6"} component={"h3"} marginTop={0}>From C $ 147</Typography>
+})
 
+export default function TourCard({tour}) {
+    const boxStyles = {
+        display: "flex", alignItems: "center"
+    }
+    return (<Grid item xs={3}>
+            <ThemeProvider theme={theme}>
+                <Paper elevation={3}>
+                    <img className={'img'} src={tour.image}/>
+                    <Box paddingX={1}>
+                        <Typography variant={'subtitle1'} component={'h2'}>
+                            {tour.name}
+                        </Typography>
+                        <Box
+                            sx={boxStyles}
+                        >
+                            <AccessTime sx={{width: 12.5}}/>
+                            <Typography variant={'body2'} component={'p'} marginLeft={0.5}>
+                                {tour.duration} hours
+                            </Typography>
+                        </Box>
+                        <Box sx={boxStyles} marginTop={3}>
+                            <Rating name="read-only" value={tour.rating} precision={0.5} readOnly size={"small"}/>
+                            <Typography variant={"body2"} component={"p"}>{tour.rating}</Typography>
+                            <Typography variant={"body3"} component={"p"} marginLeft={1.5}>({tour.numberOfReviews} reviews)</Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant={"h6"} component={"h3"} marginTop={0}>From C ${tour.price}</Typography>
+
+                        </Box>
                     </Box>
-                </Box>
-            </Paper>
-        </Grid>
-    );
+                </Paper>
+            </ThemeProvider>
+        </Grid>);
 };
